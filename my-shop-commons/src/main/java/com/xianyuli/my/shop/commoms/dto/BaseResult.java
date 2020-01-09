9b3187452dbbcc08bf7d868d8d4929b37f3d3cd3
liwang <liwanghoho@163.com>
2020-01-10 00:04:40 +0800

    统一返回对象

--- my-shop-commons/src/main/java/com/xianyuli/my/shop/commoms/dto/BaseResult.java
+++ my-shop-commons/src/main/java/com/xianyuli/my/shop/commoms/dto/BaseResult.java
@@ -21,35 +21,42 @@ import java.io.Serializable;
 @Setter
 @Getter
 public class BaseResult implements Serializable {
+    public static final int STATUS_SUCCESS = 200;
+    public static final int STATUS_FAIL = 500;
+
     private int status;
     private  String msg;
     private Object data;
 
-    public static BaseResult success(){
-        return createResult(ConstantUtils.STATUS_SUCCESS,"操作成功");
+    public static BaseResult success() {
+        return BaseResult.createResult(STATUS_SUCCESS, "操作成功", null);
     }
 
-    public static BaseResult success(String msg){
-        return createResult(ConstantUtils.STATUS_SUCCESS, msg);
+    public static BaseResult success(String message) {
+        return BaseResult.createResult(STATUS_SUCCESS, message, null);
     }
 
-    public static BaseResult fail(){
-        return createResult(ConstantUtils.STATUS_FAIL, "操作失败");
+    public static BaseResult success(String message, Object data) {
+        return BaseResult.createResult(STATUS_SUCCESS, message, data);
     }
 
-    public static BaseResult fail(String msg){
-        return createResult(ConstantUtils.STATUS_FAIL, msg);
+    public static BaseResult fail() {
+        return BaseResult.createResult(STATUS_FAIL, "操作失败", null);
     }
 
-    public static BaseResult fail(int status, String msg) {
-        return createResult(status, msg);
+    public static BaseResult fail(String message) {
+        return BaseResult.createResult(STATUS_FAIL, message, null);
     }
 
-    private static BaseResult createResult(int status,String msg) {
+    public static BaseResult fail(int status, String message) {
+        return BaseResult.createResult(status, message, null);
+    }
+    private static BaseResult createResult(int status, String message, Object data) {
         BaseResult baseResult = new BaseResult();
         baseResult.setStatus(status);
-        baseResult.setMsg(msg);
-        return  baseResult;
+        baseResult.setMsg(message);
+        baseResult.setData(data);
+        return baseResult;
     }
 
 
